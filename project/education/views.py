e46ad3bf3887199024b73771efb0b79af94ad9bc
unknown <valerijmanukyan.manukyan@yandex.ru>
2023-03-11 02:23:51 +0300

    released viewsets and urls system

--- project/education/views.py
+++ project/education/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from .serializers import *
+from .models import *
+
+
+class SchoolViewset(viewsets.ModelViewSet):
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
+
+
+class SchoolClassViewset(viewsets.ModelViewSet):
+    queryset = SchoolClass.objects.all()
+    serializer_class = SchoolClassSerializer
+
+
+class StudentViewset(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
--- project/project/urls.py
+++ project/project/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from education import views
+
+router = routers.DefaultRouter()
+router.register(r'schools', views.SchoolViewset)
+router.register(r'classes', views.SchoolClassViewset)
+router.register(r'students', views.StudentViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
