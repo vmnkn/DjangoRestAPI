bd528c1d3e635e01b43d3b2bebd86c462acf9772
unknown <valerijmanukyan.manukyan@yandex.ru>
2023-03-11 01:37:54 +0300

    creating and register models

--- project/education/models.py
+++ project/education/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    address = models.CharField(max_length=128)
+
+
+class SchoolClass(models.Model):
+    grade = models.IntegerField()
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=64)
+    school_class = models.ForeignKey(SchoolClass, on_delete=models.CASCADE)
